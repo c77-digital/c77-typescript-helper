cd4b6f94bd400d750aa5691c4b68065ec9e5c9ce
Christoph Stockinger <cstockinger@c77.digital>
2021-04-27 21:42:00 +0200

    fix: udpate

--- packages/response/response.ts
+++ packages/response/response.ts
@@ -2,9 +2,12 @@ export interface ResponseError {
   code: number
   message: string
 }
-export class Response {
-  constructor(public succeeded: boolean, public payload: <T>, public errors?: ResponseError[]) {
-
+export class Response<T> {
+  constructor(
+    public succeeded: boolean,
+    public payload: T,
+    public errors?: ResponseError[]
+  ) {
     return {
       succeeded,
       payload,
